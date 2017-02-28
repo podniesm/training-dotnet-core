99a62d0aaae4d6f41a88412202e7f460042e2eed
podniesm <podniesm@gmail.com>
2017-02-28 19:54:57 +0100

    domain infrastructure tests

---
+++ EntityFramework/test/Domain.Infrastructure.Test/SourceInfoTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace Domain.Infrastructure.Test
+{
+    public class SourceInfoTest
+    {
+        [Fact]
+        public void Save_SourceInfo_Test()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<DomainContext>();
+            optionsBuilder.UseNpgsql("User ID=postgres;Password=pass1;Host=localhost;Port=5432;Database=test;Pooling=true;");
+            using (var dbContext = new DomainContext(optionsBuilder.Options))
+            {
+                var sourceInfos = dbContext.SourceInfos.ToList();
+                Debug.WriteLine($@"Source Infos: {sourceInfos.Count}");
+                var sourceInfo = new SourceInfo
+                {
+                    Name = "Gazeta Wyborcza",
+                };
+                dbContext.Add(sourceInfo);
+                dbContext.SaveChanges();
+
+                Assert.False(false);
+            }
+        }
+    }
+}
---
+++ EntityFramework/test/Domain.Test/SourceInfoTest.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+using Xunit;
+
+namespace Domain.Test
+{
+    public class SourceInfoTest
+    {
+        [Fact]
+        public void First_Test()
+        {
+           Assert.True(1 == 1);
+        }
+    }
+}
